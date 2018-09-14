ba51d1e16eea3ff9e18202be095ac95fb79bb008
batu <ba921@nyu.edu>
2018-09-14 14:32:01 -0700

    Fixed a bug in DQN.

--- vanilla_dqn.py
+++ vanilla_dqn.py
@@ -11,26 +11,31 @@ class DQN_Agent:
         self.env = env
         self.memory = deque(maxlen=2000)
 
-        self.gamma = 0.95
+        self.gamma = 0.85
         self.epsilon = 1.0
         self.epsilon_min = 0.01
         self.epsilon_decay = 0.995
-        self.learning_rate = 0.01
+        self.learning_rate = 0.005
+        self.tau = .125
 
         self.model = self.create_model()
         self.target_model = self.create_model()
+        self.update_counter = 0
 
         self.batch_size = 32
 
+
     def create_model(self):
         inputs = tf.keras.Input(shape=self.env.observation_space.shape)
+        hidden = tf.keras.layers.Dense(24, activation=tf.nn.relu)(inputs)
         hidden = tf.keras.layers.Dense(48, activation=tf.nn.relu)(inputs)
-        hidden = tf.keras.layers.Dense(48, activation=tf.nn.relu)(hidden)
+        hidden = tf.keras.layers.Dense(24, activation=tf.nn.relu)(inputs)
         outputs = tf.keras.layers.Dense(self.env.action_space.n)(hidden)
 
         model = tf.keras.Model(inputs=inputs, outputs=outputs)
 
-        model.compile(optimizer='adam',
+        optimizer = tf.keras.optimizers.Adam(lr=self.learning_rate)
+        model.compile(optimizer=optimizer,
                       loss='mse',
                       metrics=['accuracy'])
 
@@ -41,26 +46,32 @@ class DQN_Agent:
 
 
     def replay(self):
-        batch_size = self.batch_size
-        if len(self.memory) < batch_size:
+        if len(self.memory) < self.batch_size:
             return
 
-        samples = random.sample(self.memory, batch_size)
+        samples = random.sample(self.memory, self.batch_size)
         for sample in samples:
             state, action, reward, next_state, done = sample
             current_q_values = self.target_model.predict(np.expand_dims(state, 0))
-        if done:
-            current_q_values[0][action] = reward
-        else:
-            future_q_values =self.target_model.predict(np.expand_dims(next_state, 0))
-            best_value = max(future_q_values[0])
-            current_q_values[0][action] = reward + self.gamma * best_value
+            if done:
+                current_q_values[0][action] = reward
+            else:
+                future_q_values =self.target_model.predict(np.expand_dims(next_state, 0))
+                best_value = max(future_q_values[0])
+                current_q_values[0][action] = reward + self.gamma * best_value
+            self.model.fit(np.expand_dims(state, 0), current_q_values, epochs=1, verbose=0)
 
-        self.model.fit(np.expand_dims(next_state, 0), current_q_values, epochs=1, verbose=0)
 
     def update_target(self):
         weights = self.model.get_weights()
-        self.target_model.set_weights(weights)
+        if self.tau == 0:
+            self.target_model.set_weights(weights)
+            self.update_counter = 0;
+        else:
+            target_weights = self.target_model.get_weights()
+            for i in range(len(target_weights)):
+                target_weights[i] = weights[i] * self.tau + target_weights[i] * (1 - self.tau)
+            self.target_model.set_weights(target_weights)
 
     def act(self, state):
         self.epsilon *= self.epsilon_decay
@@ -71,27 +82,30 @@ class DQN_Agent:
 
         return np.argmax(self.model.predict(np.expand_dims(state, 0)))
 
-env = gym.make("MountainCar-v0")
+env = gym.make("CartPole-v0")
+print(env.observation_space)
+print(env.action_space)
 agent = DQN_Agent(env)
 
-episodes  = 1000
+episodes  = 5000
 episodes_len = 200
-updateTargetNetwork = 1000
+update_target_network = 200
 
 for episode in range(episodes):
     cur_state = env.reset()
+    cumulative_reward = 0
     for step in range(episodes_len):
         action = agent.act(cur_state)
-        #env.render()
+        env.render()
         next_state, reward, done, _ = env.step(action)
-
+        cumulative_reward += reward
         agent.remember(cur_state, action, reward, next_state, done)
         agent.replay()
+        agent.update_target()
 
-        if(step *  episodes % updateTargetNetwork == 0):
-            agent.update_target()
         cur_state = next_state
 
         if done:
             break
-    print(f"Finsihed episode:{episode} with {'success.' if reward > 0 else 'failure.'}")
+    # print(f"Finsihed episode:{episode} with {'*********** success ***********.' if reward > 0 else 'failure.'}")
+    print(f"Finsihed episode:{episode} with reward: {cumulative_reward} and epsilon:{agent.epsilon:.3f}")
