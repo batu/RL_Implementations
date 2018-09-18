ade31240e3e9c10efb898dc58a38147c0140ba61
batu <ba921@nyu.edu>
2018-09-18 15:56:54 -0700

    updated pg

--- vanilla_policy.py
+++ vanilla_policy.py
@@ -17,18 +17,15 @@ class VanillaPolicyAgent():
 
     def __build_network(self, input_dim, output_dim, hidden_dims=[32, 32]):
         """Create a base network"""
+        input = tf.keras.layers.Input(shape=(input_dim,))
+        nn = input
+        for h_layer in hidden_dims:
+            nn = tf.keras.layers.Dense(h_layer, activation="relu")(nn)
 
-        self.X = tf.keras.Input(shape=(input_dim,))
-        net = self.X
+        output = tf.keras.layers.Dense(output_dim, activation="softmax")(nn)
 
-        for h_dim in hidden_dims:
-            net = tf.keras.layers.Dense(h_dim)(net)
-            net = tf.keras.layers.Activation("relu")(net)
+        self.model = tf.keras.Model(inputs=input, outputs=output)
 
-        net = tf.keras.layers.Dense(output_dim)(net)
-        net = tf.keras.layers.Activation("softmax")(net)
-
-        self.model = tf.keras.Model(inputs=self.X, outputs=net)
 
     def __build_train_fn(self):
         """Create a train function
@@ -40,23 +37,46 @@ class VanillaPolicyAgent():
         `self.train_fn([state, action_one_hot, discount_reward])`
         which would train the model.
         """
+        # This is the last layer of our model, the action probability distribution
         action_prob_placeholder = self.model.output
-        action_onehot_placeholder = tf.keras.backend.placeholder(shape=(None, self.output_dim),
-                                                    name="action_onehot")
-        discount_reward_placeholder = tf.keras.backend.placeholder(shape=(None,),
-                                                    name="discount_reward")
 
-        action_prob = tf.keras.backend.sum(action_prob_placeholder * action_onehot_placeholder, axis=1)
-        log_action_prob = tf.keras.backend.log(action_prob)
+        # The length of the one_hot is num_actions which is the same as output dimension.
+        action_onehot_placeholder   = tf.placeholder(shape=(None, self.output_dim), name="action_onehot", dtype=tf.float32)
 
-        loss = - log_action_prob * discount_reward_placeholder
-        loss = tf.keras.backend.mean(loss)
+        # The scalar reward value.
+        discount_reward_placeholder = tf.placeholder(shape=(None), name="discount_reward", dtype=tf.float32)
+
+        # ~~~ This part below is to calculate the loss ~~~
+        # This gives us the action probability over the one hot representation [0, 1] * [0.4, 0.6] -> [0, 0.6]
+        one_hot_action_probilities = action_prob_placeholder * action_onehot_placeholder
+
+        # combine the the one hot action probablilites sum([0, 0.6], [0.7, 0]) -> [0.6, 0.7]
+        action_prob = tf.reduce_sum(one_hot_action_probilities, axis=1)
+
+        # take the log, because of the log likelihood   [0.6, 0.7] -> [-0.6, -0.5]
+        log_action_prob = tf.log(action_prob)
+
+        # log_action_prob = tf.Print(log_action_prob, [action_onehot_placeholder], message="onehot placecholder: ",summarize=1000)
+        # log_action_prob = tf.Print(log_action_prob, [action_prob_placeholder], message="action placecholder: ",summarize=1000)
+        # log_action_prob = tf.Print(log_action_prob, [one_hot_action_probilities], message="pre_sum: ",summarize=1000)
+        # log_action_prob = tf.Print(log_action_prob, [action_prob], message="post_sum: ",summarize=1000)
+        # log_action_prob = tf.Print(log_action_prob, [log_action_prob], message="log_action_prob: ")
+        # log_action_prob = tf.Print(log_action_prob, [discount_reward_placeholder], message="discount_reward_placeholder: ", summarize=1000)
+
+        # The loss is (minimize the loss / maximize the likelihood)
+        # log action probability times the discounted reward
+        loss = -log_action_prob * discount_reward_placeholder
+        # loss = tf.Print(loss, [loss], message="loss: ")
+        loss = tf.reduce_mean(loss)
+        # loss = tf.Print(log_action_prob, [loss], message="reduced mean: ")
 
         adam = tf.keras.optimizers.Adam()
 
         updates = adam.get_updates(params=self.model.trainable_weights,
                                    loss=loss)
 
+        # Create a function that has inputs, and the update (which is defined by the adam update.)
+        # Self model input is another name for the state! Because that is the input
         self.train_fn = tf.keras.backend.function(inputs=[self.model.input,
                                            action_onehot_placeholder,
                                            discount_reward_placeholder],
@@ -88,8 +108,7 @@ class VanillaPolicyAgent():
         assert len(action_prob) == self.output_dim, "{} != {}".format(len(action_prob), self.output_dim)
         return np.random.choice(np.arange(self.output_dim), p=action_prob)
 
-
-    def fit(self, S, A, R):
+    def fit(self, states, actions, rewards):
         """Train a network
         Args:
             S (2-D Array): `state` array of shape (n_samples, state_dimension)
@@ -98,17 +117,14 @@ class VanillaPolicyAgent():
             R (1-D Array): `reward` array of shape (n_samples,)
                 A reward is given after each action.
         """
-        action_onehot = np_utils.to_categorical(A, num_classes=self.output_dim)
-        discount_reward = compute_discounted_R(R)
+        # Make an action one hot vector
+        action_onehot = np_utils.to_categorical(actions, num_classes=self.output_dim)
+        discount_reward = compute_discounted_R(rewards)
 
-        assert S.shape[1] == self.input_dim, "{} != {}".format(S.shape[1], self.input_dim)
-        assert action_onehot.shape[0] == S.shape[0], "{} != {}".format(action_onehot.shape[0], S.shape[0])
-        assert action_onehot.shape[1] == self.output_dim, "{} != {}".format(action_onehot.shape[1], self.output_dim)
-        assert len(discount_reward.shape) == 1, "{} != 1".format(len(discount_reward.shape))
+        self.train_fn([states, action_onehot, discount_reward])
 
-        self.train_fn([S, action_onehot, discount_reward])
 
-def compute_discounted_R(R, discount_rate=.99):
+def compute_discounted_R(sampled_rewards, discount_rate=.99):
     """Returns discounted rewards
     Args:
         R (1-D array): a list of `reward` at each time step
@@ -121,14 +137,15 @@ def compute_discounted_R(R, discount_rate=.99):
         >>> compute_discounted_R(R, .99) # before normalization
         [1 + 0.99 + 0.99**2, 1 + 0.99, 1]
     """
-    discounted_r = np.zeros_like(R, dtype=np.float32)
+    discounted_r = np.zeros_like(sampled_rewards, dtype=np.float32)
     running_add = 0
-    for t in reversed(range(len(R))):
-        running_add = running_add * discount_rate + R[t]
-        discounted_r[t] = running_add
+    for timestep in reversed(range(len(sampled_rewards))):
+        running_add = running_add * discount_rate + sampled_rewards[timestep]
+        discounted_r[timestep] = running_add
 
-    discounted_r -= discounted_r.mean() / discounted_r.std()
-
+    # normalization step
+    discounted_r -= discounted_r.mean()
+    discounted_r /= discounted_r.std()
     return discounted_r
 
 def run_episode(env, agent):
